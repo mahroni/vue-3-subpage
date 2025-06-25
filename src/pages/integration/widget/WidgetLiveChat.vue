eebba52fb3da97f474eecef746ebd160d03cee71
Ochaa <mushoddaqt@gmail.com>
2025-06-25 16:24:45 +0700

    feat: enhance WidgetLiveChat and WIdgetFormLayout components
    - Updated WidgetLiveChat.vue to include a new 'Call to Action' tab alongside the existing 'Welcome Dialog'.
    - Refactored layout and styling for improved consistency in WidgetLiveChat.vue.
    - Adjusted WIdgetFormLayout.vue for better structure and readability, ensuring consistent formatting and alignment of elements.

--- src/pages/integration/widget/WidgetLiveChat.vue
+++ src/pages/integration/widget/WidgetLiveChat.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="flex flex-col gap-4 w-full border-[1px] border-gray-300 rounded-2xl bg-white-100">
-        <div class="flex justify-between items-center p-4 border-b-[1px] border-gray-300 w-full">
-            <RoundedTab :tabs="tabs" v-model="activeTab" />
-        </div>
-        <template v-if="activeTab === 'Welcome Dialog'">
-            <WelcomeDialog />
-        </template>
+  <div class="bg-white-100 flex w-full flex-col gap-4 rounded-2xl border-[1px] border-gray-300">
+    <div class="flex w-full items-center justify-between border-b-[1px] border-gray-300 p-4">
+      <RoundedTab :tabs="tabs" v-model="activeTab" />
     </div>
+    <template v-if="activeTab === 'Welcome Dialog'">
+      <WelcomeDialog />
+    </template>
+    <template v-if="activeTab === 'Call to Action'">
+      <CallToAction />
+    </template>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -16,17 +19,18 @@ import ServerIcon from '../../../components/icons/ServerIcon.vue';
 import ToggleLeftIcon from '../../../components/icons/ToggleLeftIcon.vue';
 import IntegrationIcon from '../../../components/icons/IntegrationIcon.vue';
 import WelcomeDialog from './widget-builder/WelcomeDialog.vue';
+import CallToAction from './widget-builder/CallToAction.vue';
 
 interface Tab {
-    label: string;
-    icon: Component;
+  label: string;
+  icon: Component;
 }
 
 const tabs = [
-    { label: 'Welcome Dialog', icon: ServerIcon },
-    { label: 'Call to Action', icon: ToggleLeftIcon },
-    { label: 'Channels', icon: IntegrationIcon },
-] as Tab[]
+  { label: 'Welcome Dialog', icon: ServerIcon },
+  { label: 'Call to Action', icon: ToggleLeftIcon },
+  { label: 'Channels', icon: IntegrationIcon },
+] as Tab[];
 
-const activeTab = ref(tabs[0]?.label || '')
+const activeTab = ref(tabs[0]?.label || '');
 </script>
--- src/pages/integration/widget/form/WIdgetFormLayout.vue
+++ src/pages/integration/widget/form/WIdgetFormLayout.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="flex gap-4 w-full p-4">
-        <div class="flex flex-col gap-4 bg-gray-200 w-1/2 p-6 border-[1px] border-gray-300 rounded-2xl">
-            <div class="flex items-center gap-2 w-full justify-between">
-                <label class="text-base font-semibold text-text-subtitle">{{ label }}</label>
-                <Switch v-if="isSwitch" size="large" v-model="modelValue" variant="success" />
-            </div>
-            <Divider v-if="modelValue || $slots['additional-info']" />
-            <slot name="additional-info" />
-            <div class="flex flex-col gap-6" v-if="modelValue || !isSwitch">
-                <slot name="inputs" />
-            </div>
-        </div>
+  <div class="flex w-full flex-1 gap-4 p-4">
+    <div
+      class="flex w-full flex-col gap-4 rounded-2xl border-[1px] border-gray-300 bg-gray-200 p-6"
+    >
+      <div class="flex w-full items-center justify-between gap-2">
+        <label class="text-text-subtitle text-base font-semibold">{{ label }}</label>
+        <Switch v-if="isSwitch" size="large" v-model="modelValue" variant="success" />
+      </div>
+      <Divider v-if="modelValue || $slots['additional-info']" />
+      <slot name="additional-info" />
+      <div class="flex flex-col gap-6" v-if="modelValue || !isSwitch">
+        <slot name="inputs" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -20,17 +22,17 @@ import Divider from '@/components/ui/Divider.vue';
 import { computed } from 'vue';
 
 const props = defineProps<{
-    label: string;
-    modelValue?: boolean;
-    isSwitch?: boolean;
+  label: string;
+  modelValue?: boolean;
+  isSwitch?: boolean;
 }>();
 
 const emit = defineEmits<{
-    (e: 'update:modelValue', value: boolean): void;
+  (e: 'update:modelValue', value: boolean): void;
 }>();
 
 const modelValue = computed({
-    get: () => props.modelValue,
-    set: (value: boolean) => emit('update:modelValue', value)
+  get: () => props.modelValue,
+  set: (value: boolean) => emit('update:modelValue', value),
 });
 </script>
