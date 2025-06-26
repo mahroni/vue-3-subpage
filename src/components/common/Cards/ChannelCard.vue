922bf2baa331969e6e6cd4ebd75011075775b47c
Andriar Mafazan Rizkiansah <andriar@qiscus.cx>
2025-06-26 18:05:12 +0700

    feat: enhance ChannelCard component and improve channel listing
    - Updated ChannelCard component with new styling and structure, including dynamic badges for channel status.
    - Refactored ChannelPage to utilize ChannelCard for rendering channels, improving code organization and readability.
    - Added smooth scrolling behavior to the CSS for better user experience.

--- src/components/common/Cards/ChannelCard.vue
+++ src/components/common/Cards/ChannelCard.vue
@@ -1,11 +1,96 @@
 <template>
   <div
-    class="w-full max-w-sm transform rounded-lg bg-white p-6 shadow-lg transition-transform hover:scale-105"
-  >
-    <h1 class="mb-4 text-xl font-bold">Qiscus Card</h1>
-    <p class="mb-4 text-gray-700">This is a simple card component styled with Tailwind CSS.</p>
-    <slot></slot>
+    class="card bg-white-100 flex max-w-md cursor-pointer flex-col gap-3 rounded-2xl border border-gray-300 p-6 transition-colors ease-in duration-200"
+    :class="channel.type">
+    <div class="z-10 flex items-center justify-between text-sm font-semibold">
+      <div class="flex items-center gap-3">
+        <img class="h-10 w-10" :src="channel.icon" loading="lazy" />
+        {{ channel.name }}
+      </div>
+      <Badge v-if="channel.status === 'new'" intent="positive">New</Badge>
+      <Badge v-if="channel.status === 'popular'" intent="progress">Popular</Badge>
+    </div>
+    <div class="text-text-subtitle z-10 text-sm font-normal">
+      {{ channel.description }}
+    </div>
   </div>
 </template>
-<script setup></script>
-<style scoped></style>
+<script setup lang="ts">
+import Badge from '@/components/common/Badge.vue';
+
+defineProps<{
+  channel: {
+    type: string,
+    name: string,
+    icon: string,
+    status: string,
+    description: string,
+  };
+}>();   
+</script>
+<style scoped>
+.card {
+  position: relative;
+  overflow: hidden;
+}
+
+.card:hover {
+  border-color: #27b199;
+}
+
+.card::after {
+  content: '';
+  position: absolute;
+  left: -162px;
+  bottom: -54px;
+  width: 260px;
+  height: 260px;
+  filter: blur(115px);
+  opacity: 0.2;
+}
+
+.card.whatsapp::after {
+  background-image: url('https://omnichannel.qiscus.com/img/whatsapp_badge.svg');
+  background-size: cover;
+}
+
+.card.instagram::after {
+  background-image: url('https://omnichannel.qiscus.com/img/instagram_badge.svg');
+  background-size: cover;
+}
+
+.card.tiktok::after {
+  background-image: url('https://omnichannel.qiscus.com/img/tiktok_badge.svg');
+  background-size: cover;
+}
+
+.card.facebook::after {
+  background-image: url('https://omnichannel.qiscus.com/img/messenger_badge.svg');
+  background-size: cover;
+}
+
+.card.line::after {
+  background-image: url('https://omnichannel.qiscus.com/img/line_badge.svg');
+  background-size: cover;
+}
+
+.card.telegram::after {
+  background-image: url('https://omnichannel.qiscus.com/img/telegram_badge.svg');
+  background-size: cover;
+}
+
+.card.qiscus::after {
+  background-image: url('https://omnichannel.qiscus.com/img/qiscus_badge.svg');
+  background-size: cover;
+}
+
+.card.custom_channel::after {
+  background-image: url('https://omnichannel.qiscus.com/img/custom.svg');
+  background-size: cover;
+}
+
+.card.bot_integration::after {
+  background-image: url('https://omnichannel.qiscus.com/img/bot_badge.svg');
+  background-size: cover;
+}
+</style>
