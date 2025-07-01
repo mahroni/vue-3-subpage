02cebfcaa298511337077288dc92c1576d4be438
Mahroni <mahroni@qiscus.co>
2025-07-01 11:45:28 +0700

    fix(router): update query parameter keys for integration routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,13 +17,16 @@ export function createRouter(appId?: string | number): Router {
     {
       path: '/whatsapp',
       name: 'whatsapp',
-      component: QiscusListChannelView,
+      beforeEnter: () => {
+        window.location.href = `/integration?ch=whatsapp`;
+        return false;
+      },
     },
     {
       path: '/instagram',
       name: 'instagram',
       beforeEnter: () => {
-        window.location.href = `/integration?channel=instagram`;
+        window.location.href = `/integration?ch=instagram`;
         return false;
       },
     },
@@ -31,7 +34,7 @@ export function createRouter(appId?: string | number): Router {
       path: '/tiktok',
       name: 'tiktok',
       beforeEnter: () => {
-        window.location.href = `/integration?channel=tiktok`;
+        window.location.href = `/integration?ch=tiktok`;
         return false;
       },
     },
@@ -39,7 +42,7 @@ export function createRouter(appId?: string | number): Router {
       path: '/facebook',
       name: 'facebook',
       beforeEnter: () => {
-        window.location.href = `/integration?channel=facebook",`;
+        window.location.href = `/integration?ch=facebook",`;
         return false;
       },
     },
@@ -47,7 +50,7 @@ export function createRouter(appId?: string | number): Router {
       path: '/line',
       name: 'line',
       beforeEnter: () => {
-        window.location.href = `/integration?channel=line",`;
+        window.location.href = `/integration?ch=line",`;
         return false;
       },
     },
@@ -55,7 +58,7 @@ export function createRouter(appId?: string | number): Router {
       path: '/telegram',
       name: 'telegram',
       beforeEnter: () => {
-        window.location.href = `/integration?channel=telegram",`;
+        window.location.href = `/integration?ch=telegram",`;
         return false;
       },
     },
@@ -78,7 +81,7 @@ export function createRouter(appId?: string | number): Router {
       path: '/custom_channel',
       name: 'custom_channel',
       beforeEnter: () => {
-        window.location.href = `/integration?channel=custom_channel`;
+        window.location.href = `/integration?ch=custom_channel`;
         return false;
       },
     },
@@ -86,7 +89,7 @@ export function createRouter(appId?: string | number): Router {
       path: '/bot_integration',
       name: 'bot_integration',
       beforeEnter: () => {
-        window.location.href = `/integration?channel=bot_integration`;
+        window.location.href = `/integration?ch=bot_integration`;
         return false;
       },
     },
