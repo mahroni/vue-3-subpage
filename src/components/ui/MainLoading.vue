2cd43d0cb187b9d1fe9d182357a8aa039563b5f3
Andriar Mafazan Rizkiansah <andriar@qiscus.cx>
2025-07-03 20:35:46 +0700

    feat(ui): add MainLoading component for loading animation

---
+++ src/components/ui/MainLoading.vue
@@ -0,0 +1,14 @@
+<template>
+  <teleport to="body">
+    <div class="fixed inset-0 z-50 flex items-center justify-center bg-white/80 backdrop-blur-xs">
+      <div>
+        <Animate :source="loading" />
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script setup lang="ts">
+import loading from '@/assets/lottie/loading.json';
+import Animate from '../common/Animate.vue';
+</script>
