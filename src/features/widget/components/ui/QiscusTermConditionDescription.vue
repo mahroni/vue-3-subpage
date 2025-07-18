ae84a6d0eeff43bd44bc2212730004a5d7e5cca3
Ochaa <mushoddaqt@gmail.com>
2025-07-18 10:31:26 +0700

    refactor(Collapsible): improve template structure and styling; add QiscusTermConditionDescription component for enhanced security messaging in WidgetDetailView

---
+++ src/features/widget/components/ui/QiscusTermConditionDescription.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-text-subtitle flex flex-col gap-8">
+    <div>
+      Enabling this setting enhances the security of conversations on the Widget channel. Customers
+      will not be able to see the conversation history if they use a different device or browser.
+      However, it's essential to be aware that this may impact your total Monthly Active Users (MAU)
+    </div>
+    <div class="text-text-title bg-surface-highlight rounded-lg p-4 font-medium">
+      This feature becomes accessible starting from Qiscus Omnichannel iOS Widget v2.3.0, and Qiscus
+      Omnichannel Android Widget v2.2.0.
+    </div>
+  </div>
+</template>
